7d803d1c29fae50a10723b8fa993f24143d0aa86
itsamankrsingh <itsamankrsingh@gmail.com>
2021-08-06 23:42:39 +0530

    L5

---
+++ 05_variables.php
@@ -0,0 +1,9 @@
+<?php
+    //variables start with $ sign
+    //Variables are case sensitive
+    $name="Aman";
+    $income=50000;
+    echo "This guy is $name and his income is ₹$income<br>";
+    echo "$name is a good boy<br>";
+    echo "$name  is the real gangsta<br>";
+?>
