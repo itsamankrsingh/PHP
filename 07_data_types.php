b5e0b293c2bf42e88fa53b55adf3d6deddccdb11
itsamankrsingh <itsamankrsingh@gmail.com>
2021-08-07 23:50:10 +0530

    L7

---
+++ 07_data_types.php
@@ -0,0 +1,16 @@
+<?php
+$x=true;
+$y=false;
+echo var_dump($x);
+echo "<br>";
+echo var_dump($y);
+echo "<br>";
+ 
+$friends=array("phoebe","joey","rachel","ross","chandler","monica");
+echo var_dump($friends);
+echo "<br>";
+ 
+
+$name=NULL;
+echo var_dump($name);
+?>
