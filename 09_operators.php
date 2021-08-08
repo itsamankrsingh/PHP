e8fa6158340cbf926cc37871da13b8aaf129c1b9
itsamankrsingh <itsamankrsingh@gmail.com>
2021-08-08 23:38:20 +0530

    L9

---
+++ 09_operators.php
@@ -0,0 +1,19 @@
+<?php
+    /*operators in PHP
+    1.Arithmetic Operator
+    2.Assignment Operator
+    3.Comparison Operator
+    4.Logical Operator
+    */
+
+    $a=11;
+    $b=3;
+
+    echo "For a+b, the result is ".($a+$b)."<br>";
+    echo "For a-b, the result is ".($a-$b)."<br>";
+    echo "For a*b, the result is ".($a*$b)."<br>";
+    echo "For a/b, the result is ".($a/$b)."<br>";
+    echo "For a%b, the result is ".($a%$b)."<br>";
+    echo "For a**b, the result is ".($a**$b)."<br>";
+
+?>
